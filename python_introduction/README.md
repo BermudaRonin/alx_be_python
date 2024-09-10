b777ea0b1e92c2a1d13a0181f7e5f0cf944cdb09
Naoufal <bermudaRonin@gmail.com>
2024-09-10 01:01:41 +0100

    :tada: Add basic_operations.py

---
+++ python_introduction/README.md
@@ -0,0 +1,2 @@
+## basic_operations.py  
+Arithmetic operations in Python.
---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1, number2 = 10, 5
+
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(number1 + number2) + ".")
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(number1 / number2) + ".")
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(number1 * number2) + ".")
