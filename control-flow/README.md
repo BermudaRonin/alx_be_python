cc9e7234d913818c6c87e0728c2af338746dbbcd
Naoufal <bermudaRonin@gmail.com>
2024-09-18 07:37:16 +0100

    :tada: Add match_case_calculator.py

--- control-flow/README.md
+++ control-flow/README.md
@@ -1,3 +1,5 @@
 ## weather_advice.py 
 Utilize conditional statements to guide program execution based on user input regarding weather conditions.
 
+## match_case_calculator.py
+This calculator will prompt the user to enter two numbers and select an operation (addition, subtraction, multiplication, or division). The script will then perform the selected operation using a Match Case statement and display the result.
---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,19 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+operation = input("Choose the operation (+, -, *, /):")
+
+match operation:
+    case "+":
+        result = num1 + num2
+        print("The result is " + str(result) + ".")
+    case "-":
+        result = num1 - num2
+        print("The result is " + str(result) + ".")
+    case "*":
+        result = num1 + num2
+        print("The result is " + str(result) + ".")
+    case "/":
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        result = num1 / num2
+        print("The result is " + str(result) + ".")
