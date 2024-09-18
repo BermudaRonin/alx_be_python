b75dd63e3ef208352ebb254e1e30da70d3544e60
Naoufal <bermudaRonin@gmail.com>
2024-09-18 09:54:15 +0100

    :bug: Edit daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -6,10 +6,17 @@ priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
 prefix = "Reminder" if time_bound == "yes" else "Note"
+
+message = ""
 match priority:
     case "high" :
-        print(f"{prefix} : '{task}' is a {priority} priority task that requires immediate attention today!")
+        message = f"'{task}' is a {priority} priority task that requires immediate attention today!"
     case "medium" | "low" :
-        print(f"{prefix} : '{task}' is a {priority} priority task. Consider completing it when you have free time.")
+        message = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
     case _ :
         print(f"Wrong priority, Must be one of {"/".join(property_enum)}")
+
+if time_bound == "yes":
+    print("Reminder : " + message)
+else:
+    print("Note : " + message)
