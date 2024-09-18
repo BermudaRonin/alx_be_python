59381e25f3c66bbc98e2b9fb3c11f56f5a5ec446
Naoufal <bermudaRonin@gmail.com>
2024-09-18 07:47:43 +0100

    :bug: Fix multiplication_table.py

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,4 @@
 number = int(input("Enter a number to see its multiplication table:"))
 
-for x in range(10):
-    multiplicator = x + 1
-    result = number * multiplicator
-    print(str(number) + " * " + str(multiplicator) + " = " + str(result))
+for x in range(1, 11):
+    print(str(number) + " * " + str(x) + " = " + str(number * x))
