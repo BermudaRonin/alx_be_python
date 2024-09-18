58f0f0d410348169920742d92d60f4582a5a58bc
Naoufal <bermudaRonin@gmail.com>
2024-09-18 10:12:32 +0100

    :bug: Improve daily_reminder.py to align with requirements

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,22 +1,28 @@
-property_enum = ["high", "midium", "low"]
-time_bound_enum = ["yes", "no"]
-
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-prefix = "Reminder" if time_bound == "yes" else "Note"
-
-message = ""
-match priority:
-    case "high" :
-        message = f"'{task}' is a {priority} priority task that requires immediate attention today!"
-    case "medium" | "low" :
-        message = f"'{task}' is a {priority} priority task. Consider completing it when you have free time."
-    case _ :
-        print(f"Wrong priority, Must be one of {"/".join(property_enum)}")
+prefix = "Note"
+body = f"'{task}' is a {priority} priority task"
+separator = " "
+message = "that requires immediate attention today!"
 
 if time_bound == "yes":
-    print("Reminder : " + message)
+    prefix = "Reminder"
+    separator = " "
+    message = "that requires immediate attention today!"
 else:
-    print("Note : " + message)
+    match priority:
+        case "high":
+            prefix = "Reminder"
+            separator = " "
+            message = "that requires immediate attention today!"
+        case "medium":
+            prefix = "Note"
+            message = "schedule it for later"
+        case "low":
+            prefix = "Note"
+            separator = ". "
+            message = "Consider completing it when you have free time."
+
+print(prefix + ": " + body + separator + message)
