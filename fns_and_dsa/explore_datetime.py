4c77de8edd75c647550a6e601c60801312195722
Naoufal <bermudaRonin@gmail.com>
2024-09-28 17:08:42 +0100

    :bug: Wrapped code in functions

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,10 +1,9 @@
 from datetime import datetime, timedelta
 
-current_date = datetime.now()
+def display_current_datetime():
+    print(f"Current date and time: {datetime.now()}")
 
-print(f"Current date and time: {current_date}")
-
-days = int(input("Enter the number of days to add to the current date: "))
-future_date = current_date + timedelta(days=days)
-
-print("Future date:", future_date.strftime("%Y-%m-%d"))
+def calculate_future_date():
+    days = int(input("Enter the number of days to add to the current date: "))
+    future_date = datetime.now() + timedelta(days=days)
+    print("Future date:", future_date.strftime("%Y-%m-%d"))
