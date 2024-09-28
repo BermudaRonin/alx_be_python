f3888cc9aa6bd27a86bdcc2582d81653b8ceff0b
Naoufal <bermudaRonin@gmail.com>
2024-09-28 15:28:01 +0100

    :bug: Removed Enum

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,27 +1,13 @@
-from enum import Enum
-
-
-class Operation(Enum):
-    ADD = "add"
-    SUBTRACT = "subtract"
-    MULTIPLY = "multiply"
-    DIVIDE = "divide"
-
-    @classmethod
-    def toString(cls) -> str:
-        return ", ".join(operation.value for operation in cls)
-
-
 def perform_operation(num1, num2, operation):
-    if operation == Operation.ADD.value:
+    if operation == "add":
         return num1 + num2
-    elif operation == Operation.DIVIDE.value:
+    elif operation == "subtract":
         return num1 - num2
-    elif operation == Operation.MULTIPLY.value:
+    elif operation == "multiply":
         return num1 * num2
-    elif operation == Operation.SUBTRACT.value:
+    elif operation == "divide":
         if num2 == 0:
             return "Division by zero is not allowed"
         return num1 / num2
     else:
-        return "Invalid operation" + Operation.toString()
+        return "Invalid operation"
