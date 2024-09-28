7fe3c0fea73791728b8b66f820bb1f805d57e52e
Naoufal <bermudaRonin@gmail.com>
2024-09-28 15:14:40 +0100

    :bug: Switch to if/else instead of match/case

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,27 @@
-def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 == 0:
-                return "Division by zero is not allowed"
-            return num1 / num2
-        case _:
-            return "Invalid operation"
+from enum import Enum
+
+
+class Operation(Enum):
+    ADD = "add"
+    SUBTRACT = "subtract"
+    MULTIPLY = "multiply"
+    DIVIDE = "divide"
+
+    @classmethod
+    def toString(cls) -> str:
+        return ", ".join(operation.value for operation in cls)
+
+
+def perform_operation(num1: float, num2: float, operation: str):
+    if operation == Operation.ADD.value:
+        return num1 + num2
+    elif operation == Operation.DIVIDE.value:
+        return num1 - num2
+    elif operation == Operation.MULTIPLY.value:
+        return num1 * num2
+    elif operation == Operation.SUBTRACT.value:
+        if num2 == 0:
+            return "Division by zero is not allowed"
+        return num1 / num2
+    else:
+        return "Invalid operation" + Operation.toString()
