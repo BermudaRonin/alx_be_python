3da050629072b9cfe0d1290b320bc4ca0d358709
Bermuda Ronin <bermudaronin@gmail.com>
2024-10-04 00:34:45 +0100

    :bug: Remove results vars

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,26 +1,21 @@
 from simple_calculator import SimpleCalculator
 import unittest
 
+
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
         self.calc = SimpleCalculator()
 
     def test_addition(self):
-        result = self.calc.add(1, 1)
-        self.assertEqual(result, 2)
+        self.assertEqual(self.calc.add(1, 1), 2)
 
     def test_subtraction(self):
-        result = self.calc.subtract(1, 1)
-        self.assertEqual(result, 0)
+        self.assertEqual(self.calc.subtract(1, 1), 0)
 
     def test_multiply(self):
-        result = self.calc.multiply(1, 1)
-        self.assertEqual(result, 1)
+        self.assertEqual(self.calc.multiply(1, 1), 1)
 
     def test_divide(self):
-        result = self.calc.divide(1, 1)
-        self.assertEqual(result, 1)
-
-        result2 = self.calc.divide(1, 0)
-        self.assertIsNone(result2)
+        self.assertEqual(self.calc.divide(1, 1), 1)
+        self.assertIsNone(self.calc.divide(1, 0))
