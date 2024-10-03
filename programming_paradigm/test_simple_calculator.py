a844d04f6e602d3733721565b0415a1a07858175
Bermuda Ronin <bermudaronin@gmail.com>
2024-10-04 00:35:20 +0100

    :bug: Edit functions names

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,7 +13,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(1, 1), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(1, 1), 1)
 
     def test_divide(self):
