95d792efadd20ae1756a8a6ee0e584311871cca7
Bermuda Ronin <bermudaronin@gmail.com>
2024-10-14 00:49:30 +0100

    :bug: Fix library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,19 +1,30 @@
 
 class Book:
+
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 
 class EBook(Book):
+
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, Page Count: {self.file_size}KB"
+
 class PrintBook(Book):
+
     def __init__(self, title, author, page_count ):
         super().__init__(title, author)
         self.page_count = page_count 
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
 class Library:
@@ -26,10 +37,5 @@ class Library:
     @classmethod
     def list_books(self):
         for book in  self.books:
-            if isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            elif isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, Page Count: {book.file_size}KB")
-            else :
-                print(f"Book: {book.title} by {book.author}")
+            print(book)
 
